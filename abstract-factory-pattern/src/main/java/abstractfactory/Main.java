f6948dbf0111eb0a1a92353fc81cb62feac0c540
John Kiragu <mutuakiragu@gmail.com>
2019-08-21 14:43:27 +0300

    Make the project executable

---
+++ abstract-factory-pattern/src/main/java/abstractfactory/Main.java
@@ -0,0 +1,11 @@
+package abstractfactory;
+
+public class Main {
+
+    //Does this entail such works for coming up along here
+    //For usage and this is what happens when such values come along for usage
+    public static void main(String[] args) {
+        System.out.println("Hallo world");
+
+    }
+}
